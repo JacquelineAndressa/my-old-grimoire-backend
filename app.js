8ad1d4a809da81aac17bc10f97bf5598d9cf8b9b
Jacqueline Roignant <jacqueline.andressa2408@gmail.com>
2026-06-04 16:19:15 +0200

    Add PUT and DELETE routes

--- app.js
+++ app.js
@@ -51,4 +51,27 @@ app.get("/api/books/:id", (req, res, next) => {
     .catch((error) => res.status(404).json({ error: error }));
 });
 
+app.put("/api/books/:id", (req, res, next) => {
+  const book = new Book({
+    _id: req.params.id,
+    title: req.body.title,
+    author: req.body.author,
+    imageUrl: req.body.imageUrl,
+    year: req.body.year,
+    genre: req.body.genre,
+    ratings: req.body.ratings,
+    averageRating: req.body.averageRating,
+    userId: req.body.userId,
+  });
+  Book.updateOne({ _id: req.params.id }, book)
+    .then(() => res.status(200).json({ message: "Book updated successfully!" }))
+    .catch((error) => res.status(400).json({ error: error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Book deleted!" }))
+    .catch((error) => res.status(400).json({ error: error }));
+});
+
 module.exports = app;
