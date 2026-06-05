d516f3b3488493316a0dbe78bed1ecc72f229ded
Jacqueline Roignant <jacqueline.andressa2408@gmail.com>
2026-06-05 16:17:53 +0200

    Reorganize backend structure with routes and controllers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const Book = require("./models/book");
+const bookRoutes = require("./routes/books");
 
 const app = express();
 
@@ -22,56 +22,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/books", (req, res, next) => {
-  const book = new Book({
-    title: req.body.title,
-    author: req.body.author,
-    imageUrl: req.body.imageUrl,
-    year: req.body.year,
-    genre: req.body.genre,
-    ratings: req.body.ratings,
-    averageRating: req.body.averageRating,
-    userId: req.body.userId,
-  });
-  book
-    .save()
-    .then(() => res.status(201).json({ message: "Book saved successfully!" }))
-    .catch((error) => res.status(400).json({ error: error }));
-});
-
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error: error }));
-});
-
-app.get("/api/books/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error: error }));
-});
-
-app.put("/api/books/:id", (req, res, next) => {
-  const book = new Book({
-    _id: req.params.id,
-    title: req.body.title,
-    author: req.body.author,
-    imageUrl: req.body.imageUrl,
-    year: req.body.year,
-    genre: req.body.genre,
-    ratings: req.body.ratings,
-    averageRating: req.body.averageRating,
-    userId: req.body.userId,
-  });
-  Book.updateOne({ _id: req.params.id }, book)
-    .then(() => res.status(200).json({ message: "Book updated successfully!" }))
-    .catch((error) => res.status(400).json({ error: error }));
-});
-
-app.delete("/api/books/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Book deleted!" }))
-    .catch((error) => res.status(400).json({ error: error }));
-});
+app.use("/api/books", bookRoutes);
 
 module.exports = app;
---
+++ routes/books.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const bookCtrl = require("../controllers/books");
+
+router.post("/", bookCtrl.createBook);
+router.get("/", bookCtrl.getAllBooks);
+router.get("/:id", bookCtrl.getOneBook);
+router.put("/:id", bookCtrl.modifyBook);
+router.delete("/:id", bookCtrl.deleteBook);
+
+module.exports = router;
