bd7d921b262188b1e593efbaeb130c9917df1572
Jacqueline Roignant <jacqueline.andressa2408@gmail.com>
2026-06-09 17:32:23 +0200

    Add Sharp image optimization and remove mongoose-unique-validator

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,20 +1,31 @@
 const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
 
 const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
+  "image/jpg": "webp",
+  "image/jpeg": "webp",
+  "image/png": "webp",
+  "image/webp": "webp",
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
-  },
-});
+const storage = multer.memoryStorage();
 
-module.exports = multer({ storage: storage }).single("image");
+module.exports = (req, res, next) => {
+  multer({ storage: storage }).single("image")(req, res, (err) => {
+    if (err) return next(err);
+    if (!req.file) return next();
+
+    const filename = req.file.originalname.split(" ").join("_").split(".")[0] + Date.now() + ".webp";
+
+    sharp(req.file.buffer)
+      .resize(800, 500, { fit: "inside", withoutEnlargement: true })
+      .webp({ quality: 80 })
+      .toFile(path.join(__dirname, "..", "images", filename))
+      .then(() => {
+        req.file.filename = filename;
+        next();
+      })
+      .catch((error) => next(error));
+  });
+};
